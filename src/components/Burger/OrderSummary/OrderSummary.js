71530ecbe95b261c85d7811e77266fc1dfe7fa1a
Bahadir Kirdan <bahadir.kirdan@tum.de>
2019-03-27 16:39:32 +0100

    Add total price into OrderSummary component

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Aux from "../../../hoc/Aux";
+import Button from "../../UI/Button/Button";
 
 const orderSummary = props => {
   const ingredientSummary = Object.keys(props.ingredients).map(key => {
@@ -17,9 +18,16 @@ const orderSummary = props => {
       <h3>Your Order</h3>
       <p>A delicious burger with the following ingrediends:</p>
       <ul>{ingredientSummary}</ul>
+      <p>
+        <strong>Total Price: {props.price.toFixed(2)}$</strong>
+      </p>
       <p>Continue to Checkout?</p>
-      <button>CANCEL</button>
-      <button>CONTINUE</button>
+      <Button buttonType={"Danger"} clicked={props.purchaseCancelled}>
+        CANCEL
+      </Button>
+      <Button buttonType={"Success"} clicked={props.purchaseContinued}>
+        CONTINUE
+      </Button>
     </Aux>
   );
 };
