7ee4d273aa0fc078dc38cb953a883073a1df0e69
Bahadir Kirdan <bahadir.kirdan@tum.de>
2019-03-27 14:22:23 +0100

    Add addIngredientHandler

--- src/components/Burger/BuildControls/BuildControl/BuildControl.js
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -2,30 +2,13 @@ import React from "react";
 
 import styles from "./BuildControl.module.css";
 
-const controls = [
-  {
-    label: "Salad",
-    type: "salad"
-  },
-  {
-    label: "Bacon",
-    type: "bacon"
-  },
-  {
-    label: "Cheese",
-    type: "cheese"
-  },
-  {
-    label: "Meat",
-    type: "meat"
-  }
-];
-
 const buildControl = props => (
   <div className={styles.BuildControl}>
     <div className={styles.Label}>{props.label}</div>
     <button className={styles.Less}>Less</button>
-    <button className={styles.More}>More</button>
+    <button className={styles.More} onClick={props.added}>
+      More
+    </button>
   </div>
 );
 
