8040067e00d88724f4e596046fce39b031446b41
Bahadir Kirdan <bahadir.kirdan@tum.de>
2019-03-27 22:49:33 +0100

    Fix rendering Modal component by just checking wheter it is shown

--- src/components/Burger/OrderSummary/OrderSummary.js
+++ src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,35 +1,38 @@
-import React from "react";
+import React, { Component } from "react";
 
 import Aux from "../../../hoc/Aux";
 import Button from "../../UI/Button/Button";
 
-const orderSummary = props => {
-  const ingredientSummary = Object.keys(props.ingredients).map(key => {
-    return (
-      <li key={key}>
-        <span style={{ textTransform: "capitalize" }}>
-          {key}: {props.ingredients[key]}
-        </span>
-      </li>
-    );
-  });
-  return (
-    <Aux>
-      <h3>Your Order</h3>
-      <p>A delicious burger with the following ingrediends:</p>
-      <ul>{ingredientSummary}</ul>
-      <p>
-        <strong>Total Price: {props.price.toFixed(2)}$</strong>
-      </p>
-      <p>Continue to Checkout?</p>
-      <Button buttonType={"Danger"} clicked={props.purchaseCancelled}>
-        CANCEL
-      </Button>
-      <Button buttonType={"Success"} clicked={props.purchaseContinued}>
-        CONTINUE
-      </Button>
-    </Aux>
-  );
-};
+class OrderSummary extends Component {
+  render() {
+    const ingredientSummary = Object.keys(this.props.ingredients).map(key => {
+      return (
+        <li key={key}>
+          <span style={{ textTransform: "capitalize" }}>
+            {key}: {this.props.ingredients[key]}
+          </span>
+        </li>
+      );
+    });
 
-export default orderSummary;
+    return (
+      <Aux>
+        <h3>Your Order</h3>
+        <p>A delicious burger with the following ingrediends:</p>
+        <ul>{ingredientSummary}</ul>
+        <p>
+          <strong>Total Price: {this.props.price.toFixed(2)}$</strong>
+        </p>
+        <p>Continue to Checkout?</p>
+        <Button buttonType={"Danger"} clicked={this.props.purchaseCancelled}>
+          CANCEL
+        </Button>
+        <Button buttonType={"Success"} clicked={this.props.purchaseContinued}>
+          CONTINUE
+        </Button>
+      </Aux>
+    );
+  }
+}
+
+export default OrderSummary;
