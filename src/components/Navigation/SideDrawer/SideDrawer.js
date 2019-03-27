54a9bfa63a9438508def29528e800cddd06cca64
Bahadir Kirdan <bahadir.kirdan@tum.de>
2019-03-27 22:22:18 +0100

    Add SideDrawerToggle

--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -3,18 +3,29 @@ import React from "react";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
 import styles from "./SideDrawer.module.css";
+import Backdrop from "../../UI/Backdrop/Backdrop";
+import Aux from "../../../hoc/Aux";
 
 const sideDrawer = props => {
-  return (
-    <div className={styles.SideDrawer}>
-      <div className={styles.Logo}>
-        <Logo />
-      </div>
+  let attachedStyles = [styles.SideDrawer, styles.Close];
 
-      <nav>
-        <NavigationItems />
-      </nav>
-    </div>
+  if (props.open) {
+    attachedStyles = [styles.SideDrawer, styles.Open];
+  }
+
+  return (
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed} />
+      <div className={attachedStyles.join(" ")}>
+        <div className={styles.Logo}>
+          <Logo />
+        </div>
+
+        <nav>
+          <NavigationItems />
+        </nav>
+      </div>
+    </Aux>
   );
 };
 
