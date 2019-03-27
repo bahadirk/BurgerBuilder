d0a03808675eb3a574d39b8988ed05b1a8de194c
Bahadir Kirdan <bahadir.kirdan@tum.de>
2019-03-27 13:10:38 +0100

    Style Burger ingredients

--- src/components/Burger/BurgerIngredient/BurgerIngredient.js
+++ src/components/Burger/BurgerIngredient/BurgerIngredient.js
@@ -9,7 +9,7 @@ class BurgerIngredient extends Component {
 
     switch (this.props.type) {
       case "bread-bottom":
-        ingredient: <div className={styles.BreadBottom} />;
+        ingredient = <div className={styles.BreadBottom} />;
         break;
       case "bread-top":
         ingredient = (
